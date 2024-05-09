d6e30f9a960c2db310c4f68c318d0e60c383ba0c
blackfenzer <6431346921@student.chula.ac.th>
2024-05-09 23:16:15 +0700

    t1

--- main.py
+++ main.py
@@ -66,21 +66,21 @@ st.subheader("termRank Visualization")
 st.write(topic_model.visualize_term_rank())
 st.write(topic_model.visualize_term_rank(log_scale=True))
 
-# st.subheader("hierarchical_topics Visualization")
-# hierarchical_topics = topic_model.hierarchical_topics(docs)
-# topic_model.visualize_hierarchical_documents(
-#     docs, hierarchical_topics, embeddings=embeddings
-# )
+st.subheader("hierarchical_topics Visualization")
+hierarchical_topics = topic_model.hierarchical_topics(docs)
+topic_model.visualize_hierarchical_documents(
+    docs, hierarchical_topics, embeddings=embeddings
+)
+# Reduce dimensionality of embeddings, this step is optional but much faster to perform iteratively:
+reduced_embeddings = UMAP(
+    n_neighbors=10, n_components=2, min_dist=0.0, metric="cosine"
+).fit_transform(embeddings)
+st.write(
+    topic_model.visualize_hierarchical_documents(
+        docs, hierarchical_topics, reduced_embeddings=reduced_embeddings
+    )
+)
 
-# # Reduce dimensionality of embeddings, this step is optional but much faster to perform iteratively:
-# reduced_embeddings = UMAP(
-#     n_neighbors=10, n_components=2, min_dist=0.0, metric="cosine"
-# ).fit_transform(embeddings)
-# st.write(
-#     topic_model.visualize_hierarchical_documents(
-#         docs, hierarchical_topics, reduced_embeddings=reduced_embeddings
-#     )
-# )
 
 # st.subheader("Mat Visualization")
 # topic_distr, topic_token_distr = topic_model.approximate_distribution(
